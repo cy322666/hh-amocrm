31ad25dd4e3a6ea76211f6b124d851ee1d437a59
cy322666 <cy322666@gmail.com>
2023-04-05 12:13:55 +0200

    prepare test hook

--- app/Http/Controllers/HHController.php
+++ app/Http/Controllers/HHController.php
@@ -5,13 +5,15 @@ namespace App\Http\Controllers;
 use App\Jobs\RespondSend;
 use App\Models\Respond;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class HHController extends Controller
 {
     public function hook(Request $request)
     {
-        $respond = Respond::query()->create($request->toArray());
-
-        RespondSend::dispatch($respond);
+        Log::info(__METHOD__, $request->toArray());
+//        $respond = Respond::query()->create($request->toArray());
+//
+//        RespondSend::dispatch($respond);
     }
 }
